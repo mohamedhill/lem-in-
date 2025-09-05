32990dec2ac541834949ee804260d8675bcb14aa
boulhaj <awlhajb@gmail.com>
2025-09-05 01:01:43 +0100

    lem-in

--- funcs/parseinput.go
+++ funcs/parseinput.go
@@ -14,9 +14,9 @@ func ParseInput(scanner *bufio.Scanner) (*Graph, error) {
 	for scanner.Scan() {
 		Line++
 		line := scanner.Text()
-		line = strings.TrimSpace(line)
-		// Ignoring Emptylines and comments 
-		if line == "" || !strings.HasPrefix(line, "##") && strings.HasPrefix(line, "#") {
+
+		// Ignoring Emptylines and comments
+		if strings.TrimSpace(line) == "" || !strings.HasPrefix(line, "##") && strings.HasPrefix(line, "#") {
 			continue
 		}
 		if strings.HasPrefix(line, "##") {
@@ -28,43 +28,56 @@ func ParseInput(scanner *bufio.Scanner) (*Graph, error) {
 				fmt.Println("error in input check again; usage: ##start||##end")
 				return nil, nil
 			}
-			if Line == 0 {
-				Ants, err := strconv.Atoi(line)
-				if Ants <= 0 || err != nil {
-					fmt.Println("Error: Check the number of Ants; must be a positive integer number")
+		}
+		if Line == 0 {
+			Ants, err := strconv.Atoi(line)
+			if Ants <= 0 || err != nil {
+				fmt.Println("Error: Check the number of Ants; must be a positive integer number")
+				return nil, nil
+			}
+			Graph.Ants = Ants
+		}
+		if Line > 0 {
+			if strings.Contains(line, "-") {
+				parts := strings.Split(line, "-")
+				if len(parts) != 2 {
+					fmt.Println("error in input check again; usage of links: example1-example2")
 					return nil, nil
 				}
-				Graph.Ants = Ants
-			}
-			if Line > 0 {
-				if strings.Contains(line, "-") {
-					parts := strings.Split(line, "-")
-					if len(parts) != 2 {
-						fmt.Println("error in input check again; usage of links: example1-example2")
-						return nil, nil
-					}
-
-				} else {
-					splitted := strings.Fields(line)
-					if len(splitted) != 3 {
-						fmt.Println("error in input check again; usage of rooms: roomname x y")
-						return nil, nil
-					}
-					if strings.HasPrefix(splitted[0], "#") || strings.HasPrefix(splitted[0], "L") {
-						fmt.Println("error in input check again; room name can't start with L or #")
-						return nil, nil
-					}
-					x, err := strconv.Atoi(splitted[1])
-					if err != nil {
-						fmt.Println("error in input checkroom cordinates")
-					}
-					y, err := strconv.Atoi(splitted[2])
-					if err != nil {
-						fmt.Println("error in input checkroom cordinates")
-					}
-					if
-
+				room1, exist1 := Graph.Rooms[parts[1]]
+				room2, exist2 := Graph.Rooms[parts[2]]
+				if !exist1 || !exist2 {
+					fmt.Println("error room can't be linked (dosen't exist)")
+					return nil, nil
 				}
+				room1.links = append(room1.links, room2)
+				room2.links = append(room2.links, room1)
+			} else {
+				splitted := strings.Fields(line)
+				if len(splitted) != 3 {
+					fmt.Println("error in input check again; usage of rooms: roomname x y")
+					return nil, nil
+				}
+				if strings.HasPrefix(splitted[0], "#") || strings.HasPrefix(splitted[0], "L") {
+					fmt.Println("error in input check again; room name can't start with L or #")
+					return nil, nil
+				}
+				cordinate1, err := strconv.Atoi(splitted[1])
+				if err != nil {
+					fmt.Println("error in input checkroom cordinates")
+				}
+				cordinate2, err := strconv.Atoi(splitted[2])
+				if err != nil {
+					fmt.Println("error in input checkroom cordinates")
+				}
+				for _, v := range Graph.Rooms {
+					if splitted[0] == v.Name {
+						fmt.Println("Error Duplicate room names")
+						return nil, nil
+					}
+				}
+				room := &Room{Name: splitted[0], x: cordinate1, y: cordinate2}
+
 			}
 		}
 	}
